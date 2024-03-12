3c0b452344d945cb89bc6c27779991efaa3334da
gao-sz <gaoszzzz@qq.com>
2024-03-12 11:39:31 +0800

    blog url

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -2,16 +2,16 @@ const siteConfig = {
   title: "Nextjs Contentlayer Blog",
   description:
     "A static minimalistic blog template powered by Next.js and Contentlayer.",
-  author: "Saurabh Charde",
+  author: "gao-sz",
   url: "https://nextjs-contentlayer-tailwind.vercel.app",
   siteImage: "/images/announcement-banner.png",
   socials: {
-    github: "https://github.com/schardev",
-    linkedin: "https://linkedin.com/in/scharde",
-    twitter: "https://twitter.com/saurabhcharde",
-    telegram: "https://t.me/saurabhcharde",
-    email: "saurabhchardereal@gmail.com",
-    site: "https://schar.dev",
+    github: "https://github.com/gao-sz",
+    linkedin: "https://github.com/gao-sz",
+    twitter: "https://github.com/gao-sz",
+    telegram: "https://github.com/gao-sz",
+    email: "https://github.com/gao-sz",
+    site: "https://nextjs-contentlayer-blog-demo.vercel.app",
   },
   blog: {
     postPerPage: 10,
